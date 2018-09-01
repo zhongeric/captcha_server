4447c88e153f4d22f4d13a6a86a95afd9d88a03f
Eric Zhong <ezhong1900@gmail.com>
2018-09-01 14:46:35 -0400

    Update newServer.py

--- newServer.py
+++ newServer.py
@@ -24,7 +24,7 @@ def main():
 @app.route("/success", methods=['GET','POST'])
 def success():
     if request.method == "POST":
-        counter ++ 1
+        #counter + 1
         timestamp = request.form.get('timestamp', '')
         product = request.form.get('product-title', '')
         string_formatted = 'Product Success ' + str(product) + ' [' + str(timestamp) + ']'
