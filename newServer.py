9f6df07ad64af7d879d3fc54ad9e07fc0f8be471
Eric Zhong <ezhong1900@gmail.com>
2018-09-01 14:24:34 -0400

    Update newServer.py

--- newServer.py
+++ newServer.py
@@ -31,7 +31,7 @@ def success():
         success.append(string_formatted)
         return(success)
     if request.method == "GET":
-        return(render_template('success.html', content = str(success)))
+        return(render_template(str(success)))
 
 @app.route('/json', methods=['GET'])
 def json():
