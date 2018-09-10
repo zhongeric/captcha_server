dd3be1194b729004e230e247b18b5490013a1ad9
Eric Zhong <ezhong1900@gmail.com>
2018-09-09 21:43:17 -0400

    Update newServer.py

--- newServer.py
+++ newServer.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from threading import Thread
 from sys import argv
 import logging, time, sys
