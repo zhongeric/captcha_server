f5bb68da935232f46992dbc85a015b230a7b6855
Eric Zhong <ezhong1900@gmail.com>
2018-09-15 17:08:22 -0400

    Update middleware.py

--- middleware.py
+++ middleware.py
@@ -1,11 +1,12 @@
 class headerMiddleware(object):
 
-def __init__(self, app):
-self.app = app
+    def __init__(self, app):
+        self.app = app
 
-def __call__(self, environ, start_response):
- print(‘ — — — — — — — — — — -’)
- print(‘Function called’)
- print(‘ — — — — — — — — — — -’)
- environ['host'] = 'http://www.supremenewyork.com'
- return self.app(environ, start_response)
+    def __call__(self, environ, start_response):
+        print('Function called')
+        environ['HTTP_HOST'] = 'www.supremenewyork.com'
+        environ['REMOTE_ADDR'] = 'www.supremenewyork.com'
+        environ['SERVER_NAME'] = 'www.supremenewyork.com'
+        print(environ)
+        return self.app(environ, start_response)
